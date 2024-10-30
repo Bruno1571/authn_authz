c76b6ec937d56602b91952eff30c4fa1cad6cce1
Axell505 <axelcrbl505@hotmail.com>
2024-10-30 18:35:05 -0300

    Cambios realizados en los HTML.

--- logs/urls.py
+++ logs/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from logs import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='logs/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path("register/", views.register, name="register"),
 ]
