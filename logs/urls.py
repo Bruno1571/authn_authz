011d1d8e934b155c8da9be5a6d2a32086da0d68e
Bruno1571 <brunoconde29@gmail.com>
2024-10-30 17:59:44 -0300

    registro

--- logs/urls.py
+++ logs/urls.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from logs import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='logs/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path("register/", views.register, name="register"),
 ]
--- logs/views.py
+++ logs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get("username")
+        messages.success(request, f"¡Cuenta creada para {username}!")
+        return redirect("login") # Redireccionar a la página de inicio de sesión o cualquier otra página que desee
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
