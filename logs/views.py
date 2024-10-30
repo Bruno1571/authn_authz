548f5f2dee43b2c53c7943387cee10a00182c7cd
Bruno1571 <brunoconde29@gmail.com>
2024-10-30 18:20:14 -0300

    html register

--- logs/views.py
+++ logs/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
-
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get("username")
-        messages.success(request, f"¡Cuenta creada para {username}!")
-        return redirect("login") # Redireccionar a la página de inicio de sesión o cualquier otra página que desee
+        if form.is_valid():  # Verifica si el formulario es válido
+            form.save()  # Guarda el nuevo usuario
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"¡Cuenta creada para {username}!")  # Mensaje de éxito
+            return redirect("login")  # Redirecciona a la página de inicio de sesión
     else:
-        form = UserCreationForm()
-    return render(request, "register.html", {"form": form})
+        form = UserCreationForm()  # Inicializa el formulario si la solicitud es GET
+
+    return render(request, "register.html", {"form": form})  # Renderiza la plantilla con el formulario
