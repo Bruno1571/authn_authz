e7f7fb28dd0dfcf4d77db62672f26630a42a046c
Axell505 <axelcrbl505@hotmail.com>
2024-10-30 19:03:42 -0300

    Home.html creado con login requiered.

--- authn_authz/settings.py
+++ authn_authz/settings.py
@@ -123,5 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_URL = 'login'  # La URL a la que se redirige a los usuarios no autenticados
+LOGIN_REDIRECT_URL = 'home'  # La URL a la que se redirige tras iniciar sesión
+LOGOUT_REDIRECT_URL = 'login'  # La URL a la que se redirige tras cerrar sesión
--- logs/urls.py
+++ logs/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView 
 from django.urls import path
 from logs import views
+from .views import home
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path("register/", views.register, name="register"),
+    path('home/', home, name='home'), 
 ]
