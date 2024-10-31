852586d5910d5c059aaa7231f7e8f245fb94e9f8
Bruno1571 <brunoconde29@gmail.com>
2024-10-31 20:01:33 -0300

    creacion del modelo y de la migracion

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    Titulo = models.CharField(max_length=200)
+    Contenido = models.TextField()
+    Fecha_de_Publicacion = models.DateTimeField(auto_now_add=True)
+    Autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
