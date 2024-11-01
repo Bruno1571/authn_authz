1a5c49c7b639c26bf6609d7cf64059d98c8a1279
Bruno1571 <brunoconde29@gmail.com>
2024-11-01 00:53:54 -0300

    creacion de grupos y permisos

--- logs/views.py
+++ logs/views.py
@@ -3,15 +3,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Post 
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import Permission, User
+from django.contrib.contenttypes.models import ContentType
 
 
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():  # Verifica si el formulario es válido
-            form.save()  # Guarda el nuevo usuario
+            user = form.save()  # Guarda el nuevo usuario y obtiene la instancia
             username = form.cleaned_data.get("username")
             messages.success(request, f"¡Cuenta creada para {username}!")  # Mensaje de éxito
+            content_type = ContentType.objects.get(app_label='logs', model='post')
+            permiso = Permission.objects.get(codename='view_post', content_type=content_type)
+            user.user_permissions.add(permiso)  # Agrega el permiso al usuario
+
             return redirect("login")  # Redirecciona a la página de inicio de sesión
     else:
         form = UserCreationForm()  # Inicializa el formulario si la solicitud es GET
@@ -19,7 +26,8 @@ def register(request):
     return render(request, "registration/register.html", {"form": form})  # Renderiza la plantilla con el formulario
 
 
-@login_required
+@login_required 
+@permission_required('logs.add_post', raise_exception=True)
 def posts_create(request):
     if request.method == 'POST':
         title = request.POST['Titulo']
@@ -30,6 +38,7 @@ def posts_create(request):
     return render(request, 'create.html')
 
 @login_required
+@permission_required('logs.view_post', raise_exception=True)
 def home(request):
     nombreAutor = request.GET.get('autor')
     if nombreAutor:
@@ -40,10 +49,11 @@ def home(request):
             posts = []
     else:
         posts = Post.objects.all()
-
-    return render(request, 'home.html', {'posts': posts, 'nombreAutor': nombreAutor})
+    can_create = request.user.has_perm('logs.add_post')
+    return render(request, 'home.html', {'posts': posts, 'nombreAutor': nombreAutor, 'can_create': can_create})
 
 @login_required
+@permission_required('logs.delete_post', raise_exception=True)
 def posts_delete(request, post_id):
     post = get_object_or_404(Post, id=post_id)
 
@@ -54,6 +64,7 @@ def posts_delete(request, post_id):
     return render(request, 'confirmar_eliminacion.html', {'post': post})
 
 @login_required
+@permission_required('logs.change_post', raise_exception=True)
 def posts_edit(request, post_id):
     post = get_object_or_404(Post, id=post_id)
 
@@ -67,8 +78,9 @@ def posts_edit(request, post_id):
 
 
 @login_required
+@permission_required('logs.view_post', raise_exception=True)
 def posts_detail(request, post_id):
-    # Recupera el post usando el ID proporcionado. Si no se encuentra, se muestra un error 404.
     post = get_object_or_404(Post, id=post_id)
-    return render(request, 'detallePublicaciones.html', {'post': post})
-
+    can_delete = request.user.has_perm('logs.delete_post')
+    can_edit = request.user.has_perm('logs.change_post')
+    return render(request, 'detallePublicaciones.html', {'post': post, 'can_delete': can_delete, 'can_edit': can_edit})
